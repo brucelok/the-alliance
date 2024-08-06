d0547557e51a5867b12bf364a5873422cb411838
Bruce Lok <bruce.lok@hashicorp.com>
2024-08-06 10:28:19 +1000

    add secgroup and key

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ resource "aws_instance" "amazon_linux" {
   ami           = var.instance_ami
   instance_type = var.instance_type
   count         = var.instance_count
+  key_name      = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
   tags = {
     Name = random_pet.name[count.index].id
   }
--- variables.tf
+++ variables.tf
@@ -17,3 +17,13 @@ variable "instance_count" {
   description = "Number of instances"
   default     = 1
 }
+
+variable "key_name" {
+  description = "Name of the key pair to use"
+  default     = "myMacbook"
+}
+
+variable "security_group_id" {
+  description = "ID of the security group"
+  default     = "sg-05583691e895a6b4e"
+}
